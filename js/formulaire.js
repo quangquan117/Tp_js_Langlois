1caae39b3c29316cc6ee8bfa2b8e3632f5bb3f25
Leo Langlois <leo.langlois2@gmail.com>
2021-03-16 16:35:52 +0100

    new push

--- js/formulaire.js
+++ js/formulaire.js
@@ -1,21 +1,22 @@
 document.addEventListener("DOMContentLoaded", (e)=>{
     console.log("here\n");
-    let btn, warning;
+    let btn, warning, event;
     btn = document.getElementsByTagName("input")[3];
     warning = document.querySelector(".warning");
     //le nom de la class
-    let event = (e) =>{
+    event = (e) =>{
         e.preventDefault();
         let User = {
             age: document.getElementsByTagName("input")[0].value,
             prenom: document.getElementsByTagName("input")[1].value
         };
         if (!User.age || !User.prenom || !isNaN(User.age)) {
+            console.log("here\n");
             warning.style.display = "block";
             warning.classList.add("warning");
             warning.classList.remove("success");
             warning.textContent = "Champs vides: remplir les champs!";
         }
     }
-    btn.addEventListener("click", event());
+    btn.addEventListener("click", event);
 })
